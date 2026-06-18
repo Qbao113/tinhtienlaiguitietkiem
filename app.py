e101703faee056cc155ee4bfe3abb754e6a7f586
Qbao113 <23030113@student.bdu.edu.vn>
2026-06-18 15:03:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.image("logo.jpg")
 # Tiêu đề app
-st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_PHẠM QUANG BẢO")
+st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_QUANG BẢO")
 
 # Nhập dữ liệu
 C = st.number_input(
